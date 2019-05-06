d91dec460a3a8d26e8fe2ddc6e733b60ccf6eeb0
Tony Brandao <35744641+acbrandao@users.noreply.github.com>
2019-05-05 21:22:18 -0400

    changed the default path of PHP hot reload to come form templates page.

--- hotreload.js
+++ hotreload.js
@@ -9,7 +9,7 @@
 ///   Revision History:
 ///   Name:           Date:        Description:
 ///-----------------------------------------------------------------
-var php_reload_url="templates/hotreload.php";
+var php_reload_url="templates/hotreload.php";  //default to templates folder - adust to suit
 var page =location.href.split("/").slice(-1); 
 if (page=="") 
   page="index.html" ;  //defaults to index.hmtl or index.php
